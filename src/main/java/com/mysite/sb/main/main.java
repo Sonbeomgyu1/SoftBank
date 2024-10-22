1c4d879137e2f6839cd9c5f42d8bce401f442377
sonbeomgyu <sjk6285@naver.com>
2024-10-22 13:27:46 +0900

    컴퍼니.비즈니스 페이지만듬
    .

--- src/main/java/com/mysite/sb/main/main.java
+++ src/main/java/com/mysite/sb/main/main.java
@@ -7,17 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class main {
 
 	// 메인페이지
-	@GetMapping("/layout")
+	@GetMapping("/")
 	public String Main() {
 		return "main";
 	}
 
 	// 회사소개
+	@GetMapping("/company")
+	public String company() {
+		return "company";
+	}
+	//사업분야
 	@GetMapping("/business")
 	public String business() {
 		return "business";
 	}
-
 	// 헤더
 	@GetMapping("/header")
 	public String header() {
