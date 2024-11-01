4f0f9dbf5318341a78840c682ac64c546392ad5d
sonbeomgyu <sjk6285@naver.com>
2024-11-01 10:42:19 +0900

    코드정리 및 회원가입 오류수정

--- src/main/java/com/mysite/sb/user/UserService.java
+++ src/main/java/com/mysite/sb/user/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @Service
 public class UserService {
@@ -15,13 +16,21 @@ public class UserService {
     private PasswordEncoder passwordEncoder; // 암호화 인코더 주입
 
     public UserVo registerUser(UserVo userVo) {
+        // 사용자명 중복 확인
+        if (userDao.findByUsername(userVo.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 사용자명입니다.");
+        }
+
         // 비밀번호 암호화
         userVo.setPassword(passwordEncoder.encode(userVo.getPassword()));
 
         // 저장
-        UserVo savedUser = userDao.save(userVo);
-        System.out.println("저장된 UserVo 데이터: " + savedUser);
-        
-        return savedUser;
+        try {
+            UserVo savedUser = userDao.save(userVo);
+            System.out.println("저장된 UserVo 데이터: " + savedUser);
+            return savedUser;
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("데이터베이스 오류가 발생했습니다. 관리자에게 문의하세요.", e);
+        }
     }
 }
