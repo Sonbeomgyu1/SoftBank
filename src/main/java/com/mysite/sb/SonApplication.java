bcd9a0412587fc501fc3b529b87e55414fdf0e68
sonbeomgyu <sjk6285@naver.com>
2024-11-11 16:33:12 +0900

    war 디펜던시 그래들 추가 완료 헤더 수정완료

--- src/main/java/com/mysite/sb/SonApplication.java
+++ src/main/java/com/mysite/sb/SonApplication.java
@@ -2,13 +2,21 @@ package com.mysite.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-public class SonApplication {
+public class SonApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SonApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Spring Boot 애플리케이션 설정을 지정
+        return application.sources(SonApplication.class);
     }
 
+    public static void main(String[] args) {
+        // 내장 Tomcat 서버 없이 외부 서버에서 실행할 경우, main 메서드를 통해 Spring Boot 애플리케이션 시작
+        SpringApplication.run(SonApplication.class, args);
+    }
 }
